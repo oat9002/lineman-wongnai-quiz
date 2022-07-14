332a81eddb90aa5c3d8fa56b07168a359d1105d9
oat9002 <oat9002@hotmail.com>
2022-07-15 00:47:21 +0700

    :sparkles:

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "encoding/base64"
+
+func main() {
+	var whatIsIt string
+
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+
+	for i := len(sd) - 1; i >= 0; i-- {
+		whatIsIt += string(sd[i])
+	}
+
+	println(whatIsIt)
+}
